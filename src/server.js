b3e7e998040484480539645a0a09ecd5e5f29571
Michał Tułowiecki @tulski <me@tulski.com>
2020-10-31 23:08:12 +0100

    exercise 1 - solution

--- src/server.js
+++ src/server.js
@@ -12,4 +12,22 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+app.get('/users/:id', (req, res) => {
+  console.log(req.params)
+  res.send({ message: 'users' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
