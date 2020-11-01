9b7066b652b045849a3570a70edf5fa41dc6aa49
Michał Tułowiecki @tulski <me@tulski.com>
2020-11-02 00:26:22 +0100

    exercise 5 - solution

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,61 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'requires email and password' })
+  }
+  try {
+    const user = await User.create(req.body)
+    const token = await newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(400).send({ message: 'requires email and password' })
+  }
+}
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'requires email and password' })
+  }
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      return res.status(401).send({ message: 'user must be real' })
+    }
+
+    const match = await user.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'user must be real' })
+    }
+
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(400).send({ message: 'requires email and password' })
+  }
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  if (!req.headers.authorization) {
+  }
+  try {
+    const token = req.headers.authorization.split('Bearer ')[1]
+    if (!token) {
+      return res.status(401).end()
+    }
+    const payload = await verifyToken(token)
+    console.log(payload)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+    next()
+  } catch (e) {
+    console.error(e)
+    return res.status(401).end()
+  }
 }
